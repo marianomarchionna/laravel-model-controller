a71dd8604d576434c837625218fae1ceb64026a6
Mariano <marianomarchionna8@gmail.com>
2021-10-28 16:58:56 +0200

    Creazione controller e model

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{  asset('css/app.css') }}">
-    <title>@yield('title') | Movie</title>
+    <title>@yield('title') - Movie</title>
 </head>
 <body>
     @include('partials.header')
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('pagina-home');
+Route::get('/', 'MainController@listMovies')->name('pagina-home');
